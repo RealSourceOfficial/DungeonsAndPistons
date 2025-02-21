a20b875fb74c2ef021711fad257a8f88f7902858
RealSourceOfficial <codemastermind13@proton.me>
2025-02-20 17:41:45 -0800

    Original pack data. Courtesy of Captain Eric, under no copyright. Distributed to The Scurvy Dogs server first on January 21st, 2025 at 8:38pm. Any user may reasonably assume that modifications made after this point are not explicity related to or endorsed by Eric Pfau, however he still holds the right to retract his modifications entirely upon request to the author.

---
+++ kubejs/client_scripts/client_create.js
@@ -0,0 +1,8 @@
+JEIEvents.hideItems(event => {
+  event.hide('createaddition:electric_motor')
+	event.hide('createaddition:alternator')
+	event.hide('createaddition:connector')
+	event.hide('createaddition:small_light_connector')
+	event.hide('createaddition:large_connector')
+	event.hide('createaddition:tesla_coil')
+})
